6353ff170bd4ab83b2da4cdbeb71e842ad07bcba
Ram Kumar <rammaniyari@gmail.com>
2024-07-15 12:03:32 +0530

    parameterized route

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,40 @@ Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/post', function () {
-    return view('post');
+Route::get('/post/{id?}', function (string $id = null) {
+    if ($id) {
+        return "<h2>Post id is $id </h2>";
+    } else {
+        return "<h2>There is no any id exist.</h2>";
+    }
+})->whereAlpha('id');
+
+
+Route::get('/post/{id?}', function (string $id = null) {
+    if ($id) {
+        return "<h2>Post id is $id </h2>";
+    } else {
+        return "<h2>There is no any id exist.</h2>";
+    }
+})->whereNumber('id');
+
+
+Route::get('/post/{id?}', function (string $id = null) {
+    if ($id) {
+        return "<h2>Post id is $id </h2>";
+    } else {
+        return "<h2>There is no any id exist.</h2>";
+    }
+});
+
+// multiple parameter value
+
+Route::get("/post/{id?}/comment/{commentId?}", function (string $id = null, string $commentId = null) {
+    if ($id) {
+        return "<h2>Post id is $id and comment id = $commentId </h2>";
+    } else {
+        return "<h2>There is no any id exist.</h2>";
+    }
 });
 
 Route::get('/post/1', function () {
